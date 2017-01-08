a150711973e1511f0c1e7f68846b11902d2470f5
timbam <timbarbier88@gmail.com>
2017-01-08 19:10:21 +0100

    Added toggle menu functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,52 @@
 import React from 'react';
 import {Link} from 'react-router';
 
+import classNames from 'classnames';
+
 class Navbar extends React.Component {
-  render(){
-    return(
-<nav className="navbar navbar-inverse">
-  <div className="container-fluid">
-    <div className="navbar-header">
-      <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span className="sr-only">Toggle navigation</span>
-        <span className="icon-bar"></span>
-        <span className="icon-bar"></span>
-        <span className="icon-bar"></span>
-      </button>
-      <Link className="navbar-brand" to="/">TID </Link>
-    </div>
+  constructor(props) {
+    super(props);
 
-    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul className="nav navbar-nav">
-        <li><Link to="/" >Hjem</Link></li>
-        <li><Link to="/om" >Om oss</Link></li>
-        <li><Link to="/Behandlinger" >Behandlinger</Link></li>
-        <li><Link to="/Kontakt">Kontakt</Link></li>
-      </ul>
-
-    </div>
-
-  </div>
-</nav>
-    )
+    this.state = {shown: false}
+    this.onToggleClick = this.onToggleClick.bind(this);
   }
+  onToggleClick(){
+      this.setState({shown: !this.state.shown})
+  }
+
+
+  render(){
+    var classesCollapse = classNames({
+      'collapse': true,
+      'navbar-collapse': true,
+      'show': this.state.shown
+    });
+    return(
+    <nav className="navbar navbar-inverse">
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <button type="button" onClick={this.onToggleClick} className="navbar-toggle" data-toggle="collapse" data-target="#bs-navbar-collapse" aria-expanded="true" aria-controls="navbar">
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
+          <Link className="navbar-brand" to="/">TID </Link>
+        </div>
+
+        <div className={classesCollapse} id="bs-navbar-collapse">
+          <ul className="nav navbar-nav">
+            <li><Link to="/" >Hjem</Link></li>
+            <li><Link to="/Om" >Om oss</Link></li>
+            <li><Link to="/Behandlinger" >Behandlinger</Link></li>
+            <li><Link to="/Kontakt">Kontakt</Link></li>
+          </ul>
+
+        </div>
+
+      </div>
+    </nav>
+      )
+    }
 }
 export default Navbar;
